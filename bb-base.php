c7368db918b3ab33f6f61d570a35bf04ef061468
Stef Thoen <stef@baardbaard.nl>
2014-05-23 20:29:58 +0200

    Initial commit.

---
+++ bb-base.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+Plugin Name: BB Base
+Plugin URI: https://github.com/baardbaard/bb-base
+Description: Bootstart for WordPress environment for Baardbaard clients.
+Version: 1.0
+Author: Baardbaard
+Author URI: http://baardbaard.nl
+License: MIT
+*/
+
+?>
